9ff1ea874df06685c07ee175be710e2babcc326c
oawajan <oawajan@uvm.edu>
2024-03-21 15:43:21 -0400

    Fix Printing and accessing  multiple images this function allows us to better access our training features

--- main.py
+++ main.py
@@ -10,52 +10,37 @@ import json
 import cv2
 
 
-def print_hi(name)->None:
+def print_hi(name) -> None:
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def readdata(initpath) -> pd.DataFrame:
+def readdata(initpath) -> list:
     data = []
-    with open(f"{initpath}CrowdHuman_Dataset\\annotation_train.odgt") as file:
+    with open(f"{initpath}annotation_train.odgt") as file:
         for line in file:
             data.append(json.loads(line))
     return data
 
 
 def displayimages(df, count, initpath) -> None:
-    Hori = []
+
+    images = []
     for i in range(count):
         ID = df[i]['ID']
-        print(ID)
-        '''
-        try:
-            image_path = (f"{initpath}CrowdHuman_Dataset\\"
-                          f"CrowdHuman_train01\\Images\\{ID}.JPG")
-            img = cv2.imread(image_path)
-            try:
-                image_path = (f"{initpath}CrowdHuman_Dataset\\"
-                              f"CrowdHuman_train02\\Images\\{ID}.JPG")
-                img = cv2.imread(image_path)
-                try:
-                    image_path = (f"{initpath}CrowdHuman_Dataset\\"
-                                  f"CrowdHuman_train03\\Images\\{ID}.JPG")
-                    img = cv2.imread(image_path)
-                except:
-                    print("file not found in any training directoy")
-            except:
-                print("file not found in 02 training directoy")
-        except:
-            print("file not found in 01 training directoy")
-
-        Hori = np.insert(img)
-
-    cv2.imshow(f"{ID}",Hori[:])
-    cv2.waitKey(0)
-'''
+        print(df[i])
+        paths = (f"{initpath}CrowdHuman_train01\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train02\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train03\\Images\\{ID}.JPG")
+        for path in paths:
+            img = cv2.imread(path)
+            if(img is not None):
+                cv2.imshow(ID, img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    initpath = "C:\\Users\\omara\\OneDrive - University of Vermont\\"
+    initpath = "C:\\Users\\omara\\OneDrive - University of Vermont\\CrowdHuman_Dataset\\"
     df = readdata(initpath)
     displayimages(df, 5, initpath)

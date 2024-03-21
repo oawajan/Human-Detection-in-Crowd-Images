ea46da3fb5e78271eacfc8d1afc4b84cf0b7dc19
oawajan <oawajan@uvm.edu>
2024-03-21 17:10:37 -0400

    refining code

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import cv2
-import torch
+
+
+'''
+Data Ingestion
+'''
 
 
 def readdata(initpath) -> list:
@@ -19,9 +23,37 @@ def readdata(initpath) -> list:
     return data
 
 
-def displayimages(df, count, initpath) -> None:
+def readimages(data) -> list:
 
     images = []
+    # for i in range(len(data)):
+    for i in range(10):
+        ID = data[i]['ID']
+        paths = (f"{initpath}CrowdHuman_train01\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train02\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train03\\Images\\{ID}.JPG")
+        for path in paths:
+            img = cv2.imread(path)
+            if (img is not None):
+                images.append(img)
+
+    return images
+
+
+'''
+Input Image Exploration
+'''
+
+
+def displayimages(images, count, initpath) -> None:
+
+    for i in range(count):
+        cv2.imshow(str(i), images[i])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+def pixelhistogram(count, df)->None:
+
     for i in range(count):
         ID = df[i]['ID']
         print(df[i])
@@ -30,13 +62,43 @@ def displayimages(df, count, initpath) -> None:
                  f"{initpath}CrowdHuman_train03\\Images\\{ID}.JPG")
         for path in paths:
             img = cv2.imread(path)
-            if(img is not None):
-                cv2.imshow(ID, img)
+            if (img is not None):
+                vals = img.mean(axis=2).flatten()
+
+                counts,bins = np.histogram(vals,range(257))
+                plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
+                plt.xlim([-0.5, 255.5])
+                plt.xlabel('Pixel Intensity')
+                plt.ylabel('Frequency')
+                plt.title(f'Pixel Intensity Histogram {ID}')
+                plt.show()
+
+
+'''
+Data Augmentation
+'''
+
+def colortransformations(count, df)->None:
+
+    for i in range(count):
+        ID = df[i]['ID']
+        print(df[i])
+        paths = (f"{initpath}CrowdHuman_train01\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train02\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train03\\Images\\{ID}.JPG")
+        for path in paths:
+            img = cv2.imread(path)
+            if (img is not None):
+                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                cv2.imshow(ID, hsv)
                 cv2.waitKey(0)
-                cv2.destroyAllWindows()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     initpath = "C:\\Users\\omara\\OneDrive - University of Vermont\\CrowdHuman_Dataset\\"
-    df = readdata(initpath)
-    displayimages(df, 5, initpath)
+    data = readdata(initpath)
+    images = readimages(data)
+    displayimages(images, 5, initpath)
+    # pixelhistogram(5, data)
+    # colortransformations(5, data)

fc573afa58f9fbe2110d842d0f1d04c22d731f6a
rperalta14 <159787937+rperalta14@users.noreply.github.com>
2024-03-24 18:16:53 -0400

    Image Size and Objects Detected Update

--- main.py
+++ main.py
@@ -6,15 +6,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import json
 import cv2
-
 
 '''
 Data Ingestion
 '''
-
-
 def readdata(initpath) -> list:
     data = []
     with open(f"{initpath}annotation_train.odgt") as file:
@@ -22,25 +20,20 @@ def readdata(initpath) -> list:
             data.append(json.loads(line))
     return data
 
-
 def readimages(data) -> list:
 
     images = []
     # for i in range(len(data)):
     for i in range(5):
         ID = data[i]['ID']
-        paths = (f"{initpath}CrowdHuman_train01/Images/{ID}.JPG",
-                 f"{initpath}CrowdHuman_train02/Images/{ID}.JPG",
-                 f"{initpath}CrowdHuman_train03/Images/{ID}.JPG")
+        paths = (f"{initpath}CrowdHuman_train01\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train02\\Images\\{ID}.JPG",
+                 f"{initpath}CrowdHuman_train03\\Images\\{ID}.JPG")
         for path in paths:
             img = cv2.imread(path)
             if (img is not None):
                 images.append(img)
-
     return images
-
-
-
 
 '''
 Gaussian Noise Addition
@@ -54,36 +47,9 @@ def add_gaussian_noise(image, mean=0, sigma=25):
 def augment_images_with_noise(images):
     return [add_gaussian_noise(image) for image in images]
 
-
-'''
-Image Rotation
-'''
-
-def rotate_image(image, angle):
-    height, width = image.shape[:2]
-    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
-    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
-    return rotated_image
-
-def add_rotate_images(images, angle):
-    rotated_images = [rotate_image(image, angle) for image in images]
-    return rotated_images
-
-'''
-Image mirror
-'''
-
-def mirror_images(images):
-    mirrored_images = [cv2.flip(image,1) for image in images] # Flip images horizontally (along the y-axis)
-    return mirrored_images
-
-
-
 '''
 Input Image Exploration
 '''
-
-
 def displayimages(images) -> None:
 
     for i in range(len(images)):
@@ -103,11 +69,9 @@ def pixelhistogram(images)->None:
         plt.title(f'Pixel Intensity Histogram')
         plt.show()
 
-
 '''
 Data Augmentation
 '''
-
 def colortransformations(images)->None:
     i = 0
     for img in images:
@@ -116,19 +80,70 @@ def colortransformations(images)->None:
         cv2.waitKey(0)
         i += 1
 
+'''
+Image Size vs. Objects Detected
+'''
+def image_size_vs_objects(images, data) -> None:
+    sizes = [img.shape[0] * img.shape[1] for img in images]
+    objects = [len(entry['gtboxes']) for entry in data[:len(images)]]
+
+    # Scatter Plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(sizes, objects, color='blue')
+    plt.title('Image Sizes vs. Number of Objects Detected')
+    plt.xlabel('Image Size (pixels)')
+    plt.ylabel('Number of Objects Detected')
+    plt.grid(True)
+    plt.show()
+
+    # Correlation Coefficient
+    correlation_matrix = np.corrcoef(sizes, objects)
+    correlation_coefficient = correlation_matrix[0, 1]
+
+    # Correlation Coefficient Plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(sizes, objects, color='red')
+    plt.title(f'Image Sizes vs. Number of Objects Detected\nCorrelation Coefficient: {correlation_coefficient:.3f}')
+    plt.xlabel('Image Size (pixels)')
+    plt.ylabel('Number of Objects Detected')
+    plt.grid(True)
+    plt.show()
+
+    # Data for Violin Plot
+    df = pd.DataFrame({
+        'Image Size': sizes,
+        'Number of Objects Detected': objects
+    })
+
+    # Violin plot
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(x='Image Size', y='Number of Objects Detected', data=df)
+    plt.title('Distribution of Number of Objects Detected Across Image Sizes')
+    plt.xlabel('Image Size (pixels)')
+    plt.ylabel('Number of Objects Detected')
+    plt.xticks(rotation=90)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # Heatmap
+    plt.figure(figsize=(10, 6))
+    plt.hexbin(sizes, objects, gridsize=30, mincnt=1)
+    plt.colorbar(label='Count in bin')
+    plt.title('Density of Objects Detected vs. Image Size')
+    plt.xlabel('Image Size (pixels)')
+    plt.ylabel('Number of Objects Detected')
+    plt.grid(True)
+    plt.show()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    initpath = "./CrowdHuman_Dataset/"
+    initpath = ".\\CrowdHuman_Dataset\\"
     data = readdata(initpath)
     images = readimages(data)
-    #displayimages(images)
+    displayimages(images)
     noisy_images = augment_images_with_noise(images)
-    #displayimages(noisy_images, 5)  # Display augmented images
-    displayimages(noisy_images)
-    #pixelhistogram(images)
-    #colortransformations(images)
-    rotated_images = add_rotate_images(noisy_images, 30)
-    #displayimages(rotated_images)
-    mirrored_images = mirror_images(rotated_images)  # Apply mirror view
-    displayimages(mirrored_images)
-
+    displayimages(noisy_images)  # Display augmented images
+    colortransformations(images)
+    pixelhistogram(images)
+    image_size_vs_objects(images, data)
